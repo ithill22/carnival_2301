0804640e535f926c639250b11193211448271bc9
Isaac Thill <ithill22@turing.edu>
2023-02-27 09:47:26 -0700

    feat: add initialize method

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -8,14 +8,15 @@ RSpec.describe Ride do
   let(:visitor2) {Visitor.new('Tucker', 36, '$5')}
   let(:visitor3) {Visitor.new('Penny', 64, '$15')}
 
-describe "#initialize" do
-  it 'can initialize' do
-    expect(ride1).to be_instance_of(Ride)
-    expect(ride1.name).to eq('Carousel')
-    expect(ride1.min_height).to eq(24)
-    expect(ride1.admission_fee).to eq(1)
-    expect(ride1.excitement).to eq(:gentle)
-    expect(ride1.total_revenue).to eq(0)
+  describe "#initialize" do
+    it 'can initialize' do
+      expect(ride1).to be_instance_of(Ride)
+     expect(ride1.name).to eq('Carousel')
+      expect(ride1.min_height).to eq(24)
+      expect(ride1.admission_fee).to eq(1)
+      expect(ride1.excitement).to eq(:gentle)
+      expect(ride1.total_revenue).to eq(0)
+    end
   end
 
 end
