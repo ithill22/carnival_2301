604b176f996bc7244acaf4b68f6fa217bb58ca11
Isaac Thill <ithill22@turing.edu>
2023-02-27 09:25:52 -0700

    test: add test for add_preference

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 RSpec.describe Visitor do
   let(:visitor1) {Visitor.new('Bruce', 54, '$10')}
+  let(:visitor2) {Visitor.new('Tucker', 36, '$5')}
+  let(:visitor3) {Visitor.new('Penny', 64, '$15')}
 
   describe '#initialize' do
     it 'can initialize' do
@@ -9,6 +11,16 @@ RSpec.describe Visitor do
       expect(visitor1.name).to eq('Bruce')
       expect(visitor1.height).to eq(54)
       expect(visitor1.spending_money).to eq('$10')
+      expect(visitor1.preferences).to eq([])
+    end
+  end
+
+  describe '#add_preference' do
+    it 'can add symbols to the preferences array' do
+      visitor1.add_preference(:gentle)
+      visitor1.add_preference(:water)
+
+      expect(visitor1.preferences).to eq([:gentle, :water])
     end
   end
 end
