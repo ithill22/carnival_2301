9863c1576e4fcfcf63056bbbae3700ef843caf17
Isaac Thill <ithill22@turing.edu>
2023-02-27 09:41:47 -0700

    test: add test for initialize

---
+++ spec/ride_spec.rb
@@ -0,0 +1,21 @@
+require 'spec_helper'
+
+RSpec.describe Ride do
+  let(:ride1) {Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })}
+  let(:ride2) {Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })}
+  let(:ride3) {Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })}
+  let(:visitor1) {Visitor.new('Bruce', 54, '$10')}
+  let(:visitor2) {Visitor.new('Tucker', 36, '$5')}
+  let(:visitor3) {Visitor.new('Penny', 64, '$15')}
+
+describe "#initialize" do
+  it 'can initialize' do
+    expect(ride1).to be_instance_of(Ride)
+    expect(ride1.name).to eq('Carousel')
+    expect(ride1.min_height).to eq(24)
+    expect(ride1.admission_fee).to eq(1)
+    expect(ride1.excitement).to eq(:gentle)
+    expect(ride1.total_revenue).to eq(0)
+  end
+
+end
